6e8280c93117902f9dec98da1cfa9246e66755f5
tedeluri761 <152207761+tedeluri761@users.noreply.github.com>
2024-04-27 19:10:59 +0400

    add files

---
+++ index.js
@@ -0,0 +1,83 @@
+// დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი ლუწია თუ კენტი.
+// დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი დადებითია, უარყოფითი თუ ნული.
+// დაწერეთ პროგრამა, რათა შეამოწმოთ არის თუ არა რიცხვი გასაყოფი სხვა რიცხვზე.
+// დაწერეთ პროგრამა, რათა შეამოწმოთ სტრიქონი ცარიელია თუ არა.
+// დაწერეთ პროგრამა, რათა შეამოწმოთ, შეიცავს თუ არა სტრიქონი კონკრეტულ სიტყვას.
+// დაწერეთ პროგრამა, რომ იპოვოთ მინიმალური ორი რიცხვიდან.
+
+ // 11111111111)
+// function tengo(a,b){
+//     if(a % 2 == 0 ){
+//         console.log("luwia")
+//     }else{
+//         console.log("kentia")
+//     }
+    
+//     if(b % 2 == 3){
+//         console.log("luwia")
+//     }else{
+//         console.log("kentia")
+//     }
+// }
+// tengo(5,4)
+
+
+/// 2222222222222
+
+
+// function number(a){
+//     if (a < 0){
+//         console.log("uaryopitia")
+//     }else if(a === 0) {
+//     console.log("nulia")
+//     }
+//     else if(a > -1 ){
+//         console.log("dadebitia")
+
+//     }else{
+//         console.log('arasworia')
+//     }
+// }
+// number(0)
+
+/// 4444444))
+
+// function stricon(inputs){
+//     if (inputs.length === 0){
+//         console.log("carielia")
+//     }else{
+//         console.log("arariscarieli")
+//     }
+// }
+// stricon('')
+// stricon('somo text')
+
+
+// 5555555555555)))
+// let words = "tengo saxli manqana"
+// let splited = words.split(" ")
+// function tengo (a){
+//     let some = "tengo"
+// for(let i = 0; i < a.length; i++)
+// {
+//     if (a[i] === some){
+//         console.log("true")
+//     }else{
+//         console.log("false")
+//     }
+// }
+// }
+// tengo(splited)
+
+
+/// 6666666))
+// function numbersss(a,b){
+//     if(a > b){
+//         console.log(b)
+//     }else if (a < b){
+// console.log(a)
+//     }else{
+//         console.log("Error")
+//     }
+// }
+// numbersss(5,2)
